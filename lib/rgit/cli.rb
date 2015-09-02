e31b2111ded7f920f9cbf45fd5512d72b6452d7e
Ryan Benmalek <ryanai3@hotmail.com>
2015-09-02 14:30:18 -0700

    [minor] added rgit-init method_options

--- lib/rgit/cli.rb
+++ lib/rgit/cli.rb
@@ -7,6 +7,31 @@ module Rgit
     @init_descriptions = descriptions.init
 
     desc "init", "Create an empty Rgit repository or reinitialize an existing one"
+    long_desc @init_descriptions[:long_desc]
+    method_option :quiet,
+      { aliases: "q",
+        type: :boolean,
+        default: false,
+        desc: @init_descriptions[:quiet]
+      }
+    method_option :bare,
+      { type: :boolean,
+        default: false,
+        desc: @init_descriptions[:bare]
+      }
+    method_option :template,
+      { type: :string,
+        desc: @init_descriptions[:template]
+      }
+    method_option :separate_git_dir,
+      { type: :string,
+        desc: @init_descriptions[:separate_git_dir]
+      }
+    method_option :shared,
+      { type: :string,
+        enum: ["false", "true", "umask", "group", "all", "world", "everybody"],
+        desc: @init_descriptions[:shared]
+      }
     def init
 
     end
